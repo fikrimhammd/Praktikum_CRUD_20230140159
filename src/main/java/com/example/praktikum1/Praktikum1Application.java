8100ee9a464a191f052996507d1a2e1c0c59e0c4
fikrimhammd <fikri.kdi60@gmail.com>
2026-03-03 16:26:36 +0700

    menyelesaikan codingan

--- src/main/java/com/example/praktikum1/Praktikum1Application.java
+++ src/main/java/com/example/praktikum1/Praktikum1Application.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.praktikum1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Praktikum1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Praktikum1Application.class, args);
 	}
 
 }
